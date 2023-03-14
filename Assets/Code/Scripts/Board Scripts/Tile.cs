45d3168618c9b9e6e725baa12678e8dae194f2bf
Kyle Tiley <kylestiley@gmail.com>
2023-03-14 14:50:42 +0200

    Tile Placement Works

--- Assets/Code/Scripts/Board Scripts/Tile.cs
+++ Assets/Code/Scripts/Board Scripts/Tile.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    SpriteRenderer spriteRenderer;
+
+    GameObject gridManager;
+    GridManager newGridManager;
+
     [SerializeField] private GameObject _highlight;
 
+    bool notClicked = true;
+
+    private void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        newGridManager = GameObject.Find("GridManager").GetComponent<GridManager>();
+    }
+
     private void OnMouseEnter(){
-        _highlight.SetActive(true);
+        if(notClicked){
+            _highlight.SetActive(true);
+        }
     }
 
     private void OnMouseExit(){
         _highlight.SetActive(false);
+    }
+
+    private void OnMouseDown(){
+        if(notClicked){
+            if(newGridManager.playerTurn == "player1"){
+                spriteRenderer.color = Color.blue;
+                newGridManager.playerTurn = "player2";
+            }
+            else if(newGridManager.playerTurn == "player2"){
+                spriteRenderer.color = Color.red;
+                newGridManager.playerTurn = "player1";
+            }
+            else{
+                // pray for no exceptions
+            }
+            _highlight.SetActive(false);
+            notClicked = false;
+        }
     }
 }
