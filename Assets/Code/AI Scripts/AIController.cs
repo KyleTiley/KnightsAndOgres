fea7ffb7272c21a839171521f2c058c81bc461fd
Kyle Tiley <kylestiley@gmail.com>
2023-05-25 18:35:17 +0200

    Basic save state created

--- Assets/Code/AI Scripts/AIController.cs
+++ Assets/Code/AI Scripts/AIController.cs
@@ -13,15 +13,12 @@ public class AIController : MonoBehaviour
     // VARIABLES
     protected List<TileController> availableTiles = new List<TileController>();
 
+    protected int[,] boardStateArray = new int[9,9];
+
     // FUNCTIONS
     private void Awake() {
-        Debug.Log("IT DOES");
         gameController = GameController.GameControllerInstance;
         mainBoardController = gameController.mainBoardController;
-    }
-
-    protected void MiniMax(){
-        CollectAvailableTiles();
     }
 
     // might have to make this return bool
@@ -44,6 +41,30 @@ public class AIController : MonoBehaviour
                     }
                 }
             }
+        }
+    }
+
+    protected void SaveBoardState(){
+        // dont forget to clear array?
+        int arrayHorizontalIndex = 0; // determines board number
+        int arrayVerticalIndex = 0; // determines tile number on board
+
+        foreach(BoardController board in mainBoardController.boardControllers){
+            foreach(TileController tile in board.tileControllers){
+                int tileValue = 0;
+                if(tile.thisSprite.sprite == gameController.knightSprite){
+                    tileValue = 1;
+                }
+                else if(tile.thisSprite.sprite == gameController.ogreSprite){
+                    tileValue = -1;
+                }
+
+                boardStateArray[arrayHorizontalIndex, arrayVerticalIndex] = tileValue;
+
+                arrayVerticalIndex++;
+            }
+            arrayVerticalIndex = 0;
+            arrayHorizontalIndex++;
         }
     }
 
--- Assets/Code/AI Scripts/MiniMaxAI.cs
+++ Assets/Code/AI Scripts/MiniMaxAI.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class MiniMaxAI : AIController
 {
+    // private int depth = 5;
+
     // this is for minimax
+    public void PlayTurn(){
+        CollectAvailableTiles();
+
+        // int tileToPlay;
+
+        // for(int i = 0; i < availableTiles.Count; i++){
+        //     if(availableTiles[i] == ){
+
+        //     }
+        //     tileToPlay = i;
+        // }
+
+        // availableTiles[tileToPlay].OnTileClick();
+    }
 }
